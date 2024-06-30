ed6e05c56c1d9535b6799c9e0bbd879489b90ec7
karan singh D <ceitkaransingh2026@gmail.com>
2024-07-01 02:43:13 +0530

    finial push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from 'react'
+import  { useEffect, useRef, useState } from 'react'
 import "./App.css"
 import Pill from "./components/Pill"
 
@@ -7,6 +7,8 @@ const App = () => {
   const [suggestion,setsuggestion]=useState([])
   const [selectedusers,setselectedusers]=useState([])
   const [selecteduserset,setselecteduserset]=useState(new Set())
+
+  const inputref=useRef(null)
 
   useEffect(()=>{
     const fetchdata=()=>{
@@ -29,14 +31,27 @@ const App = () => {
     setselecteduserset(new Set([...selecteduserset,user.email]))
     setsuggestion([])
     setsearch("")   
+    inputref.current.focus()
 
   }
 
   function handleRemoveUser(user){
+    const updatedusers=  selectedusers.filter((selecteduser)=>selecteduser.id !==user.id)
+    setselectedusers(updatedusers);
 
+    const updatedmails= new Set(selecteduserset)
+    updatedmails.delete(user.email)
+    setselecteduserset(updatedmails)
   }
 
   console.log(selectedusers);
+  const handlekeydown=(e)=>{
+    if(e.key=== "Backspace" && e.taregt.value===""  && selectedusers.length>0){
+      const lastuser= selectedusers[selectedusers.length-1];
+      handleRemoveUser(lastuser)
+      setsuggestion([])
+    }
+  }
   
   return (
     <div className="user-search-container">
@@ -54,7 +69,7 @@ const App = () => {
           })
         }
         {/* input feild */}
-      <input type="text" value={search} onChange={(e)=>setsearch(e.target.value)} placeholder='enter the Text' />
+      <input ref={inputref} type="text" value={search} onChange={(e)=>setsearch(e.target.value)} placeholder='enter the Text' onKeyDown={handlekeydown} />
 
       <ul className="suggestions-list">
 
